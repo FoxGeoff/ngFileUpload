af0e22ea958eb1796e8bcd311ecb3d070829149f
FoxGeoff <fox.geoffrey@gmail.com>
2019-04-24 16:58:06 -0700

    Update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+// app.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+
+const UploadURL = 'http://localhost:3000/api/upload';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngFileUpload';
+export class AppComponent implements OnInit {
+  title = 'Upload a File';
+
+  public uploader: FileUploader = new FileUploader({ url: UploadURL, itemAlias: 'photo' });
+
+  ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+      console.log('FileUpload:uploaded:', item, status, response);
+      alert('File uploaded successfully');
+    };
+  }
 }
